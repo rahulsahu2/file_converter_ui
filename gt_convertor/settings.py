acebafef2f0039815fe4931fab569554600fa679
Umesh Prajapati <122529384+NoSignal000@users.noreply.github.com>
2024-03-23 12:48:37 +0530

    second

--- gt_convertor/settings.py
+++ gt_convertor/settings.py
@@ -75,6 +75,8 @@ DATABASES = {
     "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
